d94d0b85fd0f4f94aee575b3d669cf10d2a5223d
Colin Baumgard <colin.baumgard@esa.int>
2025-01-15 14:23:01 +0000

    fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Initialize and add the map
+/ Initialize and add the map
 let map;
 
 async function initMap() {
@@ -8,9 +8,8 @@ async function initMap() {
     // Request needed libraries.
     //@ts-ignore
     const { Map } = await google.maps.importLibrary("maps");
-    const { Marker } = await google.maps.importLibrary("maps");
-    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-    const { Polyline } = await google.maps.importLibrary("maps")
+    const { Marker } = await google.maps.importLibrary("marker");
+    const { Polyline } = await google.maps.importLibrary("maps");
 
     // The map, centered at Hennebont
     map = new Map(document.getElementById("map"), {
@@ -19,28 +18,29 @@ async function initMap() {
         mapId: "DEMO_MAP_ID",
     });
 
-    // The marker, positioned at Hennebont
+    // The marker, positioned at departure
     const dep_marker = new Marker({
         map: map,
         position: departure,
         title: "Depart",
-        label: { text: "01/03/2025", color: "black" }
+        label: { text: "01/03/2025", color: "black" },
     });
 
+    // The marker, positioned at last position
     const last_pos_marker = new Marker({
         map: map,
         position: last_position,
         title: "Kermadec",
-        label: "03/03/2025"
+        label: { text: "03/03/2025", color: "black" },
     });
 
     // Define the coordinates for the polyline
     const polylinePath = [
-        { lat: 47.794, lng: -3.285 },
+        departure,
         { lat: 48.8566, lng: 2.3522 }, // Paris
         { lat: 51.5074, lng: -0.1278 }, // London
         { lat: 40.7128, lng: -74.0060 }, // New York
-        departure,
+        last_position,
     ];
 
     // Create the polyline
@@ -54,7 +54,6 @@ async function initMap() {
 
     // Set the polyline on the map
     journey.setMap(map);
-
 }
 
 initMap();
